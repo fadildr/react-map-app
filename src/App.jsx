a729a53fe1a711d6d1be8c0ff01442efeb5f66c3
fadildr <drfadildr12@gmail.com>
2023-11-15 21:04:07 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.js
+
+import Map from "./components/map";
+
+function App() {
+  return (
+    <div>
+      <h1>React Map App</h1>
+      <div className="app">
+        <Map />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/map.jsx
@@ -0,0 +1,30 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const Map = () => {
+  const position = [-6.228113484071862, 106.80703103118532];
+
+  return (
+    <MapContainer
+      center={position}
+      zoom={20}
+      style={{ height: "100vh", width: "80%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          Office 8 Building, Level 18-A
+          <br />
+          Jl. Senopati no. 8 SCBD Area
+          <br />
+          Kel. Senayan, Kebayoran Baru, South Jakarta, 12190
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
